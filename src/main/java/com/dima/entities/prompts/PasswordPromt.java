a96ce8c330408735f090b9a88c9f44adeba06059
DimaProhorenko <dima.study.web@gmail.com>
2023-06-09 22:22:40 +0300

    FIX prompts error handling

--- src/main/java/com/dima/entities/prompts/PasswordPromt.java
+++ src/main/java/com/dima/entities/prompts/PasswordPromt.java
@@ -1,17 +1,34 @@
 package com.dima.entities.prompts;
 
 public class PasswordPromt extends Prompt{
-    public static final int MIN_LENGTH = 6;
-    public static final int MAX_LENGTH = 16;
-    public static final String PASSWORD_MSG = "Enter password (min - %d chars, max - %d chars)"
+    private enum Errors {
+        LESS_THAN_MIN("Password must be greater than %d chars".formatted(MIN_LENGTH)),
+        GREATER_THAN_MAX("Password must be less than %d chars".formatted(MAX_LENGTH));
+
+        String errMsg;
+        Errors(String errMsg) {
+            this.errMsg = errMsg;
+        }
+    }
+    private static final int MIN_LENGTH = 6;
+    private static final int MAX_LENGTH = 16;
+    private static final String PASSWORD_MSG = "Enter password (min - %d chars, max - %d chars)"
             .formatted(MIN_LENGTH, MAX_LENGTH);
-    public static final String PASSWORD_ERROR = "Password must be at least %d chars and at most %d chars"
-            .formatted(MIN_LENGTH, MAX_LENGTH);
+
+
     public PasswordPromt() {
-        super(PASSWORD_MSG, PASSWORD_ERROR);
+        super(PASSWORD_MSG);
     }
     @Override
     public boolean validate(String input) {
-        return input.length() >= MIN_LENGTH && input.length() <= MAX_LENGTH;
+        if (input.length() < MIN_LENGTH) {
+            setError(Errors.LESS_THAN_MIN.errMsg);
+            return false;
+        }
+        if (input.length() > MAX_LENGTH) {
+            setError(Errors.GREATER_THAN_MAX.errMsg);
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/com/dima/entities/prompts/UserNamePrompt.java
+++ src/main/java/com/dima/entities/prompts/UserNamePrompt.java
@@ -1,16 +1,33 @@
 package com.dima.entities.prompts;
 
 public class UserNamePrompt extends Prompt{
+    private enum Errors {
+        LESS_THAN_MIN("Username must be greater than %d chars".formatted(MIN_LENGTH)),
+        GREATER_THAN_MAX("Username must be less than %d chars".formatted(MAX_LENGTH));
+        String err;
+
+        Errors(String err) {
+            this.err = err;
+        }
+    }
     private static final int MIN_LENGTH = 5;
     public static final int MAX_LENGTH = 16;
     private static final String USERNAME_MSG = "Enter your username(at least %s chars, at most %s)".formatted(MIN_LENGTH, MAX_LENGTH);
     private static final String USERNAME_ERROR = "Length between %s - %s chars".formatted(MIN_LENGTH, MAX_LENGTH);
     public UserNamePrompt() {
-        super(USERNAME_MSG, USERNAME_ERROR);
+        super(USERNAME_MSG);
     }
 
     @Override
     public boolean validate(String input) {
-        return input.length() >= MIN_LENGTH && input.length() <= MAX_LENGTH;
+        if (input.length() < MIN_LENGTH) {
+            setError(Errors.LESS_THAN_MIN.err);
+            return false;
+        }
+        if (input.length() > MAX_LENGTH) {
+            setError(Errors.GREATER_THAN_MAX.err);
+            return false;
+        }
+        return true;
     }
 }
